e6cdd198b608f547d3fbb007f459f4b1ab8b5056
Adam Palmer <adampalmer@rocketmortgage.com>
2024-06-24 21:37:18 -0400

    Day 1

--- CandyShop/CandyShop/ShoppingCart.cs
+++ CandyShop/CandyShop/ShoppingCart.cs
@@ -2,38 +2,38 @@
 
 public class ShoppingCart
 {
-    public List<CandyItem> Items { get; set; }
+    //properties
+    public List<CandyItem> shoppingCartItems { get; set; }
 
-    public ShoppingCart() 
-    {
-        Items = new List<CandyItem>();
-    }
+    //constructor
 
+    //method
     public void AddItem(CandyItem item)
     {
-        Items.Add(item);    
-        Console.WriteLine($"{item.Name} added to the cart.");
+        shoppingCartItems.Add(item);
     }
 
     public void DisplayCart()
     {
-        if (Items.Count == 0)
+        if (shoppingCartItems.Count == 0)
         {
-            Console.WriteLine("\nYour cart is empty.");
-            return;
+            Console.WriteLine("Your cart is empty.");
         }
 
-        Console.WriteLine("\nItems in your cart:");
-        foreach (var item in Items)
-        {
-            Console.WriteLine($"- {item.Name}: ${item.Price} ${item.Quantity}");
+        else 
+        { 
+            Console.WriteLine("Items in your cart:");
+            foreach (var item in shoppingCartItems)
+            {
+                Console.WriteLine($"{item.Name}: ${item.Price}");
+            }
         }
     }
 
     public decimal CalculateTotal()
     {
         decimal total = 0;
-        foreach (var item in Items) 
+        foreach (var item in shoppingCartItems)
         {
             total += item.Price;
         }
@@ -43,7 +43,7 @@ public class ShoppingCart
     public void Checkout()
     {
         DisplayCart();
-        if (Items.Count == 0) 
+        if (shoppingCartItems.Count == 0)
         {
             Console.WriteLine("Nothing to checkout.");
             return;
@@ -52,40 +52,9 @@ public class ShoppingCart
         decimal total = CalculateTotal();
         Console.WriteLine($"\nTotal: ${total:F2}");
         Console.WriteLine("Thank you for your purchase!");
-        Items.Clear();
+        shoppingCartItems.Clear();
     }
+
 }
 
-/*
-static void Main(string[] args)
-{
-    ShoppingCart cart = new ShoppingCart();
-    List<CandyItem> candyStore = new List<CandyItem>
-    {
-        new CandyItem("Tootsie Rolls", 0.05m),
-        new CandyItem("Blow Pops", 0.25m),
-        new CandyItem("Jaw Breaker", 1.00m),
-        new CandyItem("Laffy Taffy", 0.10m),
-        new CandyItem("Pixie Stix", 0.05m),
-        new CandyItem("Candy Bars", 2.00m),
-        new CandyItem("Licorice", 2.50m),
-        new CandyItem("Nerds", 0.10m),
-        new CandyItem("Pop Rocks", 0.75m),
-        new CandyItem("Gum", 0.10m),
-        new CandyItem("M&Ms", 2.00m),
-        new CandyItem("Skittles", 2.00m)
-    };
 
-    while (true)
-    {
-        Console.WriteLine("\nCandy Store Menu: ");
-        for (int i = 0; i < candyStore.Count; i++)
-        {
-            Console.WriteLine($"{i = 1}. {candyStore[i].Name} - ${candyStore[i].Price:F2} each");
-        }
-        Console.WriteLine("5. View Cart");
-        Console.WriteLine("6. Checkout");
-        Console.WriteLine("7. Exit");
-    }
-}
-*/
---
+++ CandyShop/CandyShop/StoreFront.cs
@@ -0,0 +1,57 @@
+﻿namespace CandyShop;
+
+public class StoreFront
+{
+    public List<CandyItem> stockItems { get; set; } = new List<CandyItem>();    
+
+    //constructor
+    public StoreFront()
+    {
+        CandyItem tootsieRoll = new CandyItem("Tootsie Roll", 0.05m);
+        CandyItem blowPops = new CandyItem("Blow Pops", 0.25m);
+        CandyItem jawBreaker = new CandyItem("Jaw Breaker", 1.00m);
+        CandyItem laffyTaffy = new CandyItem("Laffy Taffy", 0.10m);
+        CandyItem pixieStix = new CandyItem("Pixie Stix", 0.05m);
+        CandyItem candyBar = new CandyItem("Candy Bar", 2.00m);
+        CandyItem licorice = new CandyItem("Licorice", 2.50m);
+        CandyItem nerds = new CandyItem("Nerds", 0.1m);
+        CandyItem popRocks = new CandyItem("Pop Rocks", 0.75m);
+        CandyItem gum = new CandyItem("Gum", 0.1m);
+        CandyItem mANDms = new CandyItem("M&Ms", 2.00m);
+        CandyItem skittles = new CandyItem("Skittles", 2.00m);
+
+        stockItems.Add(tootsieRoll);
+        stockItems.Add(blowPops);
+        stockItems.Add(jawBreaker);
+        stockItems.Add(laffyTaffy);
+        stockItems.Add(pixieStix);
+        stockItems.Add(licorice);
+        stockItems.Add(nerds);
+        stockItems.Add(popRocks);
+        stockItems.Add(gum);
+        stockItems.Add(mANDms);
+        stockItems.Add(skittles);
+        stockItems.Add(candyBar);
+
+    }
+
+    public void displayStoreFront()
+    {
+            if (stockItems.Count == 0)
+            {
+                Console.WriteLine("We are sold out!");
+            }
+
+            else
+            {
+            int counter = 1;
+                Console.WriteLine("Items in your storefront:");
+                foreach (var item in stockItems)
+                {
+                    Console.WriteLine($"{counter}. {item.Name}: Price: ${item.Price}");
+                    counter++;
+                }
+            }
+    }
+
+}
